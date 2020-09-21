7d3e8e42c5c49c4d5764d81d5305cf44257c406c
Serhii Bohoslavskyi <sbohoslavskyi@gmail.com>
2020-09-21 21:02:40 +0300

    Create CountdownTime component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import './App.css';
+import CountdownTimer from "../CountdownTimer";
 
 function App() {
   return (
     <div className="app">
-
+      <CountdownTimer />
     </div>
   );
 }
