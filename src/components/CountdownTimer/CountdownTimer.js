a55eabe54dac8780070f50b7a5f6a953a2b1bbe9
Serhii Bohoslavskyi <sbohoslavskyi@gmail.com>
2020-09-23 19:35:58 +0300

    Use hooks in CountdownTimer component

--- src/components/CountdownTimer/CountdownTimer.js
+++ src/components/CountdownTimer/CountdownTimer.js
@@ -1,74 +1,62 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import './CountdownTimer.css';
 
-class CountdownTimer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      remainTime: this.calculateTimeRemaining()
+export default function CountdownTimer() {
+  const [remainingTime, setRemainingTime] = useState({days: 0, hours: 0, minutes: 0, seconds: 0});
+  const [intervalId, setIntervalId] = useState(0);
+
+  useEffect(() => {
+    setIntervalId(setInterval(calculateTimeRemaining, 1000));
+
+    return () => {
+      clearInterval(intervalId);
     }
-  }
+  }, []);
 
-  componentDidMount() {
-    this.intervalId = setInterval(() => { this.tick() }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
-
-  calculateTimeRemaining() {
+  function calculateTimeRemaining() {
     const total = Date.parse(new Date(2021, 1, 1)) - Date.parse(new Date());
     const seconds = Math.floor( (total/1000) % 60 );
     const minutes = Math.floor( (total/1000/60) % 60 );
     const hours = Math.floor( (total/(1000*60*60)) % 24 );
     const days = Math.floor( total/(1000*60*60*24) );
 
-    return {
+    setRemainingTime({
       days,
       hours,
       minutes,
       seconds
-    };
+    });
   }
 
-  tick() {
-    this.setState({remainTime: this.calculateTimeRemaining()});
-  }
-
-  render() {
-    return (
-      <div className="countdown-timer">
-        <ul>
-          <li>
-            <div className="countdown-timer-item countdown-timer-item__first">
-              {this.state.remainTime.days}
-            </div>
-            <div className="countdown-timer-capture">Days</div>
-          </li>
-          <li>
-            <div className="countdown-timer-item">
-              {this.state.remainTime.hours}
-            </div>
-            <div className="countdown-timer-capture">Hours</div>
-          </li>
-          <li>
-            <div className="countdown-timer-item">
-              {this.state.remainTime.minutes}
-            </div>
-            <div className="countdown-timer-capture">Minutes</div>
-          </li>
-          <li>
-            <div className="countdown-timer-item countdown-timer-item__last">
-              {this.state.remainTime.seconds}
-            </div>
-            <div className="countdown-timer-capture">Seconds</div>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className="countdown-timer">
+      <ul>
+        <li>
+          <div className="countdown-timer-item countdown-timer-item__first">
+            {remainingTime.days}
+          </div>
+          <div className="countdown-timer-capture">Days</div>
+        </li>
+        <li>
+          <div className="countdown-timer-item">
+            {remainingTime.hours}
+          </div>
+          <div className="countdown-timer-capture">Hours</div>
+        </li>
+        <li>
+          <div className="countdown-timer-item">
+            {remainingTime.minutes}
+          </div>
+          <div className="countdown-timer-capture">Minutes</div>
+        </li>
+        <li>
+          <div className="countdown-timer-item countdown-timer-item__last">
+            {remainingTime.seconds}
+          </div>
+          <div className="countdown-timer-capture">Seconds</div>
+        </li>
+      </ul>
+    </div>
+  );
 }
-
-export default CountdownTimer;
